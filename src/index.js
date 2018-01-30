5b7ffb12a748ca38fef5c4135bb7ecc87d093bf8
娄啊 <loua@louadeMacBook-Pro.local>
2018-01-30 16:35:39 +0800

    build

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-export default 'Welcome to externals-dependencies'
+export default function (arr = []){
+    var packageJson,externals = {};
+    try {
+        var packageJsonString = fs.readFileSync(path.join(process.cwd(), './package.json'), 'utf8');
+        packageJson = JSON.parse(packageJsonString);
+    } catch (e){
+        throw 'can not find package.json'
+    }
+    var sections = ['dependencies'].concat(arr);
+    sections = new Set(sections)
+
+    var deps = {};
+    sections.forEach(function(section){
+        Object.keys(packageJson[section] || {}).forEach(function(dep){
+            externals[dep] = 'commonjs ' + dep;
+        });
+    });
+    return externals
+}
